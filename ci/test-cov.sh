cfd596a54d4b4c923be7ef163fa241098f127f3a
Timothy Lee <timmy.ccl@gmail.com>
2020-05-29 16:38:55 +0800

    Test
    Signed-off-by: Timothy Lee <timmy.ccl@gmail.com>

--- ci/test-cov.sh
+++ ci/test-cov.sh
@@ -2,10 +2,10 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-stmts=$(cat $DIR/htmlcov/status.json | jq '([.files[].index.nums[1]]| add)')
-stmts=$((stmts+$(cat $DIR/htmlcov/status.json | jq '([.files[].index.nums[1]]| add)')))
+stmts=$(cat $DIR/../chart/htmlcov/status.json | jq '([.files[].index.nums[1]]| add)')
+stmts=$((stmts+$(cat $DIR/../chart/htmlcov/status.json | jq '([.files[].index.nums[1]]| add)')))
 
-miss=$(cat $DIR/htmlcov/status.json | jq '([.files[].index.nums[3]]| add)')
+miss=$(cat $DIR/../chart/htmlcov/status.json | jq '([.files[].index.nums[3]]| add)')
 #miss=$((miss+$(cat ../modules/primehub-admission/htmlcov/status.json | jq '([.files[].index.nums[3]]| add)')))
 
 total=$(echo {'"'s'"':$stmts, '"'m'"': $miss} | jq '100 * ( ( .s - .m ) / .s ) + 0.5 | floor ')
